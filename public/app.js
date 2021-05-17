8b73a68cae65dd1b9a9c7d5e6d577a2de1df12bd
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2021-05-17 12:42:06 +0000

    ✨: adding theme switch feat

---
+++ public/app.js
@@ -0,0 +1,46 @@
+// === Theme Switcher logic here === //
+const inputs = document.querySelectorAll('input')
+const body = document.body
+
+// Function for switching theme
+const updateTheme = (theme) => {
+  switch (theme) {
+    case 'light':
+      body.classList.remove('weird')
+      body.classList.add('light')
+      break
+    case 'weird':
+      body.classList.remove('light')
+      body.classList.add('weird')
+      break
+    default:
+      body.classList.remove('weird')
+      body.classList.remove('light')
+      break
+  }
+}
+
+// We cycle through the inputs
+inputs.forEach((input) => {
+  // We first check if an input was already checked.
+  // If that's the case we update the theme accordingly
+  if (input.checked) updateTheme(input.value)
+
+  // We then listen for an input event (change) to update the theme
+  input.addEventListener('change', () => {
+    if (input.checked) updateTheme(input.value)
+  })
+})
+
+// Detect user's color preferences (between dark and light theme)
+const lightTheme = window.matchMedia('(prefers-color-scheme: light)')
+
+if (lightTheme.matches) {
+  // if the user's preferences is the light theme,
+  // we grab the input with the value of light in our 'input' nodelist
+  // and we check it
+  const lightInput = inputs[1]
+  lightInput.checked = true
+
+  updateTheme('light')
+}
