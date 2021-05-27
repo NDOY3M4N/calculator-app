6a8531cf46859cd9814666093ad48f211fcf63a5
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2021-05-27 12:23:30 +0000

    ✨: math operations

--- app.js
+++ app.js
@@ -50,3 +50,126 @@ if (lightTheme.matches) {
 
   updateTheme('light')
 }
+
+// === Arithmetic Operations logic here === //
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+}
+
+const updateDisplay = () => {
+  const screen = document.querySelector('.screen')
+  screen.innerText = calculator.displayValue
+}
+
+const inputNumber = (number) => {
+  const { displayValue, waitingForSecondOperand } = calculator
+
+  if (waitingForSecondOperand) {
+    calculator.displayValue = number
+    calculator.waitingForSecondOperand = false
+  } else {
+    calculator.displayValue =
+      displayValue === '0' ? number : displayValue + number
+  }
+}
+
+const inputDecimal = (dot) => {
+  if (calculator.waitingForSecondOperand === true) {
+    calculator.displayValue = '0.'
+    calculator.waitingForSecondOperand = false
+    return
+  }
+
+  if (!calculator.displayValue.includes(dot)) {
+    calculator.displayValue += dot
+  }
+}
+
+const inputOperator = (nextOperator) => {
+  const { firstOperand, displayValue, operator } = calculator
+
+  const inputValue = parseFloat(displayValue)
+
+  if (operator && calculator.waitingForSecondOperand) {
+    calculator.operator = nextOperator
+    return
+  }
+
+  if (firstOperand === null && !isNaN(inputValue)) {
+    calculator.firstOperand = inputValue
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator)
+
+    // calculator.displayValue = String(result)
+    calculator.displayValue = `${parseFloat(result.toFixed(7))}`
+    calculator.firstOperand = result
+  }
+
+  calculator.waitingForSecondOperand = true
+  calculator.operator = nextOperator
+}
+
+const calculate = (firstOperand, secondOperand, operator) => {
+  if (operator === '+') {
+    return firstOperand + secondOperand
+  } else if (operator === '-') {
+    return firstOperand - secondOperand
+  } else if (operator === 'x') {
+    return firstOperand * secondOperand
+  } else if (operator === '/') {
+    return firstOperand / secondOperand
+  }
+
+  return secondOperand
+}
+
+const resetCalculator = () => {
+  calculator.displayValue = '0'
+  calculator.firstOperand = null
+  calculator.waitingForSecondOperand = false
+  calculator.operator = null
+}
+
+const deleteValue = () => {
+  const { displayValue } = calculator
+  if (displayValue === '0') return
+
+  if (displayValue.length === 1) {
+    calculator.displayValue = '0'
+    return
+  }
+
+  calculator.displayValue = displayValue.slice(0, -1)
+}
+
+// Event Delegation used here
+const btns = document.querySelector('.keypad')
+btns.addEventListener('click', (evt) => {
+  const { target } = evt
+
+  if (!target.matches('button')) return
+
+  switch (target.dataset.type) {
+    case 'operator':
+      inputOperator(target.innerText)
+      break
+    case 'decimal':
+      inputDecimal(target.innerText)
+      break
+    case 'reset':
+      resetCalculator()
+      break
+    case 'delete':
+      deleteValue()
+      break
+    default:
+      inputNumber(target.innerText)
+  }
+
+  updateDisplay()
+})
+
+updateDisplay()
